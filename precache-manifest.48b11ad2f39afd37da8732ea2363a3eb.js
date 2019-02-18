fc8f419ab1c95638f2e683bd13fd90a741e9f244
Renan Brando <reebrando@gmail.com>
2019-02-17 22:58:11 -0300

    deploy

---
+++ precache-manifest.48b11ad2f39afd37da8732ea2363a3eb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b79c49aa4d3d1223700",
+    "url": "/css/chunk-76161809.b0f5c5f1.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8b79c49aa4d3d1223700",
+    "url": "/js/chunk-76161809.85f57ffd.js"
+  },
+  {
+    "revision": "5f9972a91415d6266716",
+    "url": "/js/app.8257d0bb.js"
+  },
+  {
+    "revision": "99a5c11f09776f256023",
+    "url": "/js/chunk-vendors.b03da253.js"
+  },
+  {
+    "revision": "82c7f6b7aa3b6333eb86",
+    "url": "/js/chunk-2399b842.afaf7563.js"
+  },
+  {
+    "revision": "35a610324d21372edd78",
+    "url": "/js/chunk-c80093b2.ff06dad3.js"
+  },
+  {
+    "revision": "b92482e6bb9c77e0d5f9",
+    "url": "/js/chunk-5529a634.03dcfcd1.js"
+  },
+  {
+    "revision": "5010fbdbb4f623c5d343",
+    "url": "/js/chunk-0a3d6705.9b2f304a.js"
+  },
+  {
+    "revision": "f2764752363984dfe513",
+    "url": "/js/chunk-185588aa.9e5b57fc.js"
+  },
+  {
+    "revision": "12d28e22fb72617c8db9c12eeed7bcbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99a5c11f09776f256023",
+    "url": "/css/chunk-vendors.e071cb55.css"
+  },
+  {
+    "revision": "35a610324d21372edd78",
+    "url": "/css/chunk-c80093b2.78fb55d8.css"
+  },
+  {
+    "revision": "b92482e6bb9c77e0d5f9",
+    "url": "/css/chunk-5529a634.b571f279.css"
+  },
+  {
+    "revision": "82c7f6b7aa3b6333eb86",
+    "url": "/css/chunk-2399b842.b0f5c5f1.css"
+  },
+  {
+    "revision": "f2764752363984dfe513",
+    "url": "/css/chunk-185588aa.b51acb97.css"
+  }
+];
